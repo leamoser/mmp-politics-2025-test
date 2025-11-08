364865c87b86a6906344a3026eb6939eb37adec8
Lea Moser <privat@lea-moser.ch>
2025-11-08 14:14:30 +0100

    disable lazy load

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 const config = useRuntimeConfig()
 const { story } = await useAsyncStoryblok('home', {
+  lazy: false,
   api: {
     version: config.public.storyblok_version,
     resolve_relations: 'overviewTestimonials.testimonials'
